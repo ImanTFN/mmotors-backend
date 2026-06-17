be43f990ff82c5108f4a164b04c1496d4f146d83
ImanTFN <imantoufani@gmail.com>
2026-06-17 19:44:04 +0200

    fix: update CORS for production

--- app/main.py
+++ app/main.py
@@ -16,7 +16,11 @@ app = FastAPI(title="M-Motors API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://mmotors-frontend-one.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
